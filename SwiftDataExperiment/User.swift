34bf31a5f2b870461ec8e052bd0fcce1c3b764ce
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-17 23:05:53 +0200

    Add User entity

---
+++ SwiftDataExperiment/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SwiftDataExperiment
+//
+//  Created by LennartWisbar on 17.10.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
