1012b99071a99014244d00469d686cd7151a4531
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-17 23:06:20 +0200

    Supply model container to app window

--- SwiftDataExperiment/SwiftDataExperimentApp.swift
+++ SwiftDataExperiment/SwiftDataExperimentApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataExperimentApp: App {
@@ -13,5 +14,6 @@ struct SwiftDataExperimentApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
