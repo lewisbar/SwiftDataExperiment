2aabaff7089b8d65f1b7c8dce1f9ee3961b1a7b3
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-17 21:02:16 +0200

    Initial Commit

---
+++ SwiftDataExperiment/SwiftDataExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExperimentApp.swift
+//  SwiftDataExperiment
+//
+//  Created by LennartWisbar on 17.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
