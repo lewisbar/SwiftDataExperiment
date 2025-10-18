31eef63f29725fc1bee3a623cdaded4f4b670dec
Lennart Wisbar <lewisbar.git@gmail.com>
2025-10-18 19:34:06 +0200

    Show upcoming users or every user

---
+++ SwiftDataExperiment/UsersView.swift
@@ -0,0 +1,30 @@
+//
+//  UsersView.swift
+//  SwiftDataExperiment
+//
+//  Created by LennartWisbar on 18.10.25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct UsersView: View {
+    @Query var users: [User]
+
+    var body: some View {
+        List(users) { user in
+            Text(user.name)
+        }
+    }
+
+    init(minimumJoinDate: Date) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: \User.name)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now)
+        .modelContainer(for: User.self)
+}
